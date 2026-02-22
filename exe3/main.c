c33a8190ec89aa167df9b11ba091e174b68a9b56
MatheusBraido <matheusbraido09@gmail.com>
2026-02-22 17:59:28 -0300

    teste exercicio0 variaveis

--- exe3/main.c
+++ exe3/main.c
@@ -7,8 +7,8 @@ const int BTN_PIN_VERDE = 26;
 const int LED_PIN_VERMELHO= 4;
 const int LED_PIN_VERDE = 6;
 
-bool btn_vermelho_pressed = false;
-bool btn_verde_pressed = false;
+const bool btn_vermelho_pressed = false;
+const bool btn_verde_pressed = false;
 
 int main() {
   stdio_init_all();
