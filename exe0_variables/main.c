7dd16e4b6a50366ceffef50678a5edd87261d15d
MatheusBraido <matheusbraido09@gmail.com>
2026-02-22 18:05:55 -0300

    correção ex0

--- exe0_variables/main.c
+++ exe0_variables/main.c
@@ -4,15 +4,17 @@ void foo(int *a) {
     *a = *a + 1; // acessa ponteiro
 }
 
-void main(void) {
+int main(void) {
     int a = 0;
     int b = 0;
 
-    while (1) {
-        a = foo(&a);
+    while (a<=5) {
+        foo(&a);
 
         if (a > 5) {
             b = 1;
         }
     }
+    printf("a: %d, b: %d\n", a, b);
+    return 0;
 }
